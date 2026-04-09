7286a6935a76c6d4d7f57f0e777b85b508614b59
Ivan <nexacoders2@gmail.com>
2026-04-09 15:53:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ MONGO_URI = os.getenv("MONGO_URI", "")
 DB_NAME = os.getenv("DB_NAME", "Cluster0")
 
 OWNER_ID = int(os.getenv("OWNER_ID", 0))
-BOT_USERNAME = os.getenv("BOT_USERNAME", "NomadeHelpBot")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "")
 
-SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/LearningBotsCommunity")
-UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "https://t.me/Learning_Bots")
-START_IMAGE = os.getenv("START_IMAGE", "https://files.catbox.moe/o5eekb.jpg")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "")
+UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "")
+START_IMAGE = os.getenv("START_IMAGE", "")
