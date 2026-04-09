cef11716a20bca0c6075cd8c4a7f2527efd1840b
Ivan <nexacoders2@gmail.com>
2026-04-09 16:24:42 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,6 @@
 from .start import register_handlers
 from .group_commands import register_group_commands
-#from .repo import register_repo_handler
+from .logs import register_logs
 from .forcejoin import register_forcejoin
 
 
@@ -9,5 +9,6 @@ def register_all_handlers(app):
     #register_repo_handler(app)
     register_group_commands(app)
     register_forcejoin(app)
+    register_logs(app)
 
     print("✅ All handlers registered!")
