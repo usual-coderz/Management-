41377b599dcb230daeb701d04b03c64a227dd298
Ivan <nexacoders2@gmail.com>
2026-04-09 16:04:08 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,14 +1,13 @@
 from .start import register_handlers
 from .group_commands import register_group_commands
-from .repo import register_repo_handler
-
-# 👉 Import forcejoin so decorators register ho jaye
-from . import forcejoin
+#from .repo import register_repo_handler
+from .forcejoin import register_forcejoin
 
 
 def register_all_handlers(app):
     register_handlers(app)
-    register_repo_handler(app)
+    #register_repo_handler(app)
     register_group_commands(app)
+    register_forcejoin(app)
 
-    print("✅ All handlers registered (including Force Join)!")
+    print("✅ All handlers registered!")
